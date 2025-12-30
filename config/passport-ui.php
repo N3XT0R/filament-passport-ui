7288f8f379b3c7d22978efc3433b605b8d0c1ea7
Ilya Beliaev <info@php-dev.info>
2025-12-30 22:39:20 +0100

    feat: implementing package.

--- config/passport-ui.php
+++ config/passport-ui.php
@@ -3,8 +3,6 @@
 return [
     /**
      * Model used as the owner of the OAuth2 clients.
-     *
-     * This model is used to display the owner of the client in the UI.
      */
     'owner_model' => 'App\\Models\\User',
     'owner_label_attribute' => 'name',
