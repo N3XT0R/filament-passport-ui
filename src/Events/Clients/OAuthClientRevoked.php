44935b36c7d5238ae3fe8806098433b5009cf532
Ilya Beliaev <info@php-dev.info>
2025-12-30 18:17:24 +0100

    chore: first structure for project

--- src/Events/Clients/OAuthClientRevoked.php
+++ src/Events/Clients/OAuthClientRevoked.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Events\Clients;
 
 use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Auth\Authenticatable;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 use Laravel\Passport\Client;
@@ -15,7 +16,7 @@ class OAuthClientRevoked
     use InteractsWithSockets;
     use SerializesModels;
 
-    public function __construct(public Client $channel)
+    public function __construct(public Client $client, public ?Authenticatable $actor = null)
     {
     }
 }
