56ec3b35fb5d59a5b97d8dc72695863c57edc87e
Ilya Beliaev <info@php-dev.info>
2025-12-30 22:58:20 +0100

    feat: implementing package.

---
+++ src/Resources/ClientResource/Pages/ListClients.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Pages;
+
+use Filament\Resources\Pages\ListRecords;
+use N3XT0R\FilamentPassportUi\Resources\ClientResource;
+
+class ListClients extends ListRecords
+{
+    protected static string $resource = ClientResource::class;
+}
