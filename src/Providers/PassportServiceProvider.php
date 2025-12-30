c68ef6217d721cb08bd7bac0ae48776d5287648f
Ilya Beliaev <info@php-dev.info>
2025-12-30 17:38:45 +0100

    chore: first structure for project

--- src/Providers/PassportServiceProvider.php
+++ src/Providers/PassportServiceProvider.php
@@ -5,11 +5,19 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\ClientRepository as BaseClientRepository;
 use Laravel\Passport\Passport;
+use N3XT0R\FilamentPassportUi\Repositories\ClientRepository;
 use N3XT0R\FilamentPassportUi\Services\Scopes\ScopeRegistryService;
 
 class PassportServiceProvider extends ServiceProvider
 {
+
+    public function register(): void
+    {
+        $this->app->singleton(BaseClientRepository::class, ClientRepository::class);
+    }
+
     public function boot(): void
     {
         $this->bootScopes();
