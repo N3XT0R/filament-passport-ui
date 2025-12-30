4b16967694447e06c03a7ca8665551f61e3d5c5e
Ilya Beliaev <info@php-dev.info>
2025-12-30 18:08:56 +0100

    chore: first structure for project

--- src/Observers/ClientObserver.php
+++ src/Observers/ClientObserver.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Observers;
 
 use Illuminate\Database\Eloquent\Model;
+use N3XT0R\FilamentPassportUi\Events\Clients\OAuthClientCreated;
 
 class ClientObserver extends BaseObserver
 {
     public function created(Model $model): void
     {
+        OAuthClientCreated::dispatch($model);
     }
 }
