b2651e86f9dd4057e6f2f6314a9506ca7b77421f
Ilya Beliaev <info@php-dev.info>
2025-12-31 00:16:27 +0100

    feat: implementing package.

--- resources/lang/en/passport-ui.php
+++ resources/lang/en/passport-ui.php
@@ -2,6 +2,12 @@
 
 // translations for N3XT0R/FilamentPassportUi
 return [
+    'navigation' => [
+        'group' => 'API Management',
+        'clients' => [
+            'label' => 'Clients'
+        ],
+    ],
     'common' => [
         'updated_at' => 'updated at',
         'created_at' => 'created at',
--- src/FilamentPassportUiPlugin.php
+++ src/FilamentPassportUiPlugin.php
@@ -2,11 +2,20 @@
 
 namespace N3XT0R\FilamentPassportUi;
 
-use Filament\Contracts\Plugin;
+use Filament\Contracts\Plugin as FilamentPlugin;
+use BezhanSalleh\PluginEssentials\Concerns\Plugin as Essentials;
 use Filament\Panel;
+use Filament\Support\Icons\Heroicon;
 
-class FilamentPassportUiPlugin implements Plugin
+class FilamentPassportUiPlugin implements FilamentPlugin
 {
+
+    use Essentials\BelongsToParent;
+    use Essentials\HasGlobalSearch;
+    use Essentials\HasLabels;
+    use Essentials\HasNavigation;
+    use Essentials\HasPluginDefaults;
+
     public function getId(): string
     {
         return 'filament-passport-ui';
@@ -35,5 +44,14 @@ class FilamentPassportUiPlugin implements Plugin
         $plugin = filament(app(static::class)->getId());
 
         return $plugin;
+    }
+
+    protected function getPluginDefaults(): array
+    {
+        return [
+            'navigationGroup' => __('filament-shield::filament-shield.navigation.group'),
+            'navigationLabel' => __('filament-shield::filament-shield.navigation.clients.label'),
+            'navigationIcon' => 'heroicon-o-lock-closed',
+        ];
     }
 }
