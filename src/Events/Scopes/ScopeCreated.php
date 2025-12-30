dca2960a63dac21d507d4a93ddc4b71a0d14a686
Ilya Beliaev <info@php-dev.info>
2025-12-30 18:13:40 +0100

    chore: first structure for project

--- src/Events/Scopes/ScopeCreated.php
+++ src/Events/Scopes/ScopeCreated.php
@@ -4,6 +4,13 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Events\Scopes;
 
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
 class ScopeCreated
 {
+    use Dispatchable;
+    use InteractsWithSockets;
+    use SerializesModels;
 }
