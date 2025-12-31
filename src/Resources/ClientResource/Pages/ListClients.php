6075ffa9be454997d88d3551438085569faa318d
Ilya Beliaev <info@php-dev.info>
2025-12-31 03:03:06 +0100

    feat: implementing package.

--- src/Resources/ClientResource/Pages/ListClients.php
+++ src/Resources/ClientResource/Pages/ListClients.php
@@ -4,10 +4,18 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Pages;
 
+use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource;
 
 class ListClients extends ListRecords
 {
     protected static string $resource = ClientResource::class;
+
+    public function getHeaderActions(): array
+    {
+        return [
+            CreateAction::make(),
+        ];
+    }
 }
