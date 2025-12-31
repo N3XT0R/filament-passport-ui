7fdd3f450c8fa98195e3b6dfe180990bf196ed8f
Ilya Beliaev <info@php-dev.info>
2025-12-31 01:23:56 +0100

    feat: implementing package.

--- src/Models/PassportScopeResource.php
+++ src/Models/PassportScopeResource.php
@@ -6,6 +6,7 @@ namespace N3XT0R\FilamentPassportUi\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class PassportScopeResource extends Model
 {
@@ -23,4 +24,9 @@ class PassportScopeResource extends Model
     protected $casts = [
         'is_active' => 'bool',
     ];
+
+    public function actions(): HasMany
+    {
+        return $this->hasMany(PassportScopeAction::class, 'passport_scope_resource_id');
+    }
 }
