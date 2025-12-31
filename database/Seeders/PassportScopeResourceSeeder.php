ba90da41c27113f34a2232dfc034f4dbcef4bf00
Ilya Beliaev <info@php-dev.info>
2025-12-31 01:17:59 +0100

    feat: implementing package.

--- database/Seeders/PassportScopeResourceSeeder.php
+++ database/Seeders/PassportScopeResourceSeeder.php
@@ -5,11 +5,31 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Facades\DB;
 
 class PassportScopeResourceSeeder extends Seeder
 {
     public function run(): void
     {
-        //
+        $now = Carbon::now();
+
+        $resources = [
+            [
+                'name' => 'users',
+                'description' => 'User accounts and identities',
+            ],
+        ];
+
+        foreach ($resources as $resource) {
+            DB::table('passport_scope_resources')->updateOrInsert(
+                ['name' => $resource['name']],
+                [
+                    'description' => $resource['description'],
+                    'created_at' => $now,
+                    'updated_at' => $now,
+                ]
+            );
+        }
     }
 }
