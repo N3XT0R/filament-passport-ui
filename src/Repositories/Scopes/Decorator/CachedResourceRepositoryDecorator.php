c1ad73d5d048d22df50504ab8a6dee4507fce3da
Ilya Beliaev <info@php-dev.info>
2025-12-31 01:50:47 +0100

    feat: implementing package.

--- src/Repositories/Scopes/Decorator/CachedResourceRepositoryDecorator.php
+++ src/Repositories/Scopes/Decorator/CachedResourceRepositoryDecorator.php
@@ -4,7 +4,29 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Repositories\Scopes\Decorator;
 
-class CachedResourceRepositoryDecorator
-{
+use Illuminate\Support\Collection;
+use N3XT0R\FilamentPassportUi\Models\PassportScopeResource;
+use N3XT0R\FilamentPassportUi\Repositories\Scopes\Contracts\ResourceRepositoryContract;
 
+class CachedResourceRepositoryDecorator implements ResourceRepositoryContract
+{
+    public function __construct(
+        protected ResourceRepositoryContract $innerRepository,
+    ) {
+    }
+
+    public function all(): Collection
+    {
+        return $this->innerRepository->all();
+    }
+
+    public function active(): Collection
+    {
+        return $this->innerRepository->active();
+    }
+
+    public function findByName(string $name): ?PassportScopeResource
+    {
+        return $this->innerRepository->findByName($name);
+    }
 }
