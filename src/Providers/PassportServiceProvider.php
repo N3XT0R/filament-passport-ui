4b7df6afedc82cb0f61e539de408f181cedf1dc4
Ilya Beliaev <info@php-dev.info>
2025-12-31 02:01:56 +0100

    feat: implementing package.

--- src/Providers/PassportServiceProvider.php
+++ src/Providers/PassportServiceProvider.php
@@ -8,13 +8,18 @@ use Illuminate\Support\ServiceProvider;
 use Laravel\Passport\ClientRepository as BaseClientRepository;
 use Laravel\Passport\Passport;
 use N3XT0R\FilamentPassportUi\Repositories\ClientRepository;
+use N3XT0R\FilamentPassportUi\Repositories\Scopes\ActionRepository;
+use N3XT0R\FilamentPassportUi\Repositories\Scopes\Decorator\CachedActionRepositoryDecorator;
 use N3XT0R\FilamentPassportUi\Services\Scopes\ScopeRegistryService;
+use N3XT0R\FilamentPassportUi\Repositories\Scopes\Contracts\ActionRepositoryContract;
+use Illuminate\Contracts\Container\Container as Application;
 
 class PassportServiceProvider extends ServiceProvider
 {
     public function register(): void
     {
         $this->app->singleton(BaseClientRepository::class, ClientRepository::class);
+        $this->registerRepositories();
     }
 
     public function boot(): void
@@ -34,5 +39,23 @@ class PassportServiceProvider extends ServiceProvider
         }
 
         Passport::tokensCan($scopeRegistryService->all()->toArray());
+    }
+
+    protected function registerRepositories(): void
+    {
+        $this->app->bind(
+            ActionRepositoryContract::class,
+            function (Application $app, array $params = []) {
+                $repository = $app->make(ActionRepository::class);
+
+                if (($params['cache'] ?? false) || true === (bool)config('passport-ui.cache.enabled', false)) {
+                    $repository = new CachedActionRepositoryDecorator(
+                        innerRepository: $repository,
+                    );
+                }
+
+                return $repository;
+            }
+        );
     }
 }
