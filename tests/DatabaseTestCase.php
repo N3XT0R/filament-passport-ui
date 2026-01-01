cfbc6d138c360945b68b16776be027bb398140ba
Ilya Beliaev <info@php-dev.info>
2026-01-02 00:08:26 +0100

    feat: implementing package.

--- tests/DatabaseTestCase.php
+++ tests/DatabaseTestCase.php
@@ -4,8 +4,10 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Tests;
 
+use Illuminate\Contracts\Config\Repository;
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 use Orchestra\Testbench\Attributes\WithMigration;
+use Workbench\App\Models\User;
 
 #[WithMigration]
 class DatabaseTestCase extends TestCase
@@ -13,8 +15,21 @@ class DatabaseTestCase extends TestCase
     use LazilyRefreshDatabase;
 
 
-    public function getEnvironmentSetUp($app): void
+    protected function defineEnvironment($app): void
     {
-        $app['config']->set('database.default', 'testing');
+        $config = $app->make(Repository::class);
+
+        $config->set([
+            'auth.defaults.provider' => 'users',
+            'auth.providers.users.model' => User::class,
+            'auth.guards.api' => ['driver' => 'passport', 'provider' => 'users'],
+            'database.default' => 'testing',
+            'database.connections.testing' => [
+                'driver' => 'sqlite',
+                'database' => ':memory:',
+                'prefix' => '',
+                'foreign_key_constraints' => false,
+            ],
+        ]);
     }
 }
