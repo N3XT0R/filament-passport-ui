5d03e972892b81b9bbbabf7b7cc86c5b942cfbff
Ilya Beliaev <info@php-dev.info>
2026-01-01 23:54:56 +0100

    feat: implementing package.

--- workbench/app/Providers/Filament/AdminPanelProvider.php
+++ workbench/app/Providers/Filament/AdminPanelProvider.php
@@ -2,7 +2,6 @@
 
 namespace Workbench\App\Providers\Filament;
 
-use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\AuthenticateSession;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -18,6 +17,7 @@ use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
 use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use N3XT0R\FilamentPassportUi\FilamentPassportUiPlugin;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +53,9 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 //
+            ])
+            ->plugins([
+                FilamentPassportUiPlugin::make(),
             ]);
     }
 }
