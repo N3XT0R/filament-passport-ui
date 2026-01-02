c682804a6cf1a534c582d5ec8574f21e1f7ebc83
Ilya Beliaev <info@php-dev.info>
2026-01-02 03:54:53 +0100

    feat: implementing package.

--- src/FilamentPassportUiServiceProvider.php
+++ src/FilamentPassportUiServiceProvider.php
@@ -104,7 +104,7 @@ class FilamentPassportUiServiceProvider extends PackageServiceProvider
         FilamentIcon::register($this->getIcons());
 
         // Handle Stubs
-        if (app()->runningInConsole()) {
+        if ($this->app->runningInConsole()) {
             foreach (app(Filesystem::class)->files(__DIR__ . '/../stubs/') as $file) {
                 $this->publishes([
                     $file->getRealPath() => base_path("stubs/filament-passport-ui/{$file->getFilename()}"),
@@ -123,12 +123,6 @@ class FilamentPassportUiServiceProvider extends PackageServiceProvider
     protected function bootObserver(): void
     {
         Client::observe(ClientObserver::class);
-    }
-
-    public function bootingPackage(): void
-    {
-        $this->registerRepositories();
-        $this->registerOAuthStrategies();
     }
 
     protected function getAssetPackageName(): ?string
@@ -240,13 +234,13 @@ class FilamentPassportUiServiceProvider extends PackageServiceProvider
             );
 
             $allowedTypes = array_map(
-                static fn (string $value): OAuthClientType => OAuthClientType::from($value),
+                static fn(string $value): OAuthClientType => OAuthClientType::from($value),
                 $allowedTypeValues
             );
 
             $strategies = collect($app->tagged('filament-passport-ui.oauth.strategies'))
                 ->filter(function (OAuthClientCreationStrategyInterface $strategy) use ($allowedTypes) {
-                    return array_any($allowedTypes, fn (OAuthClientType $type): bool => $strategy->supports($type));
+                    return array_any($allowedTypes, fn(OAuthClientType $type): bool => $strategy->supports($type));
                 })
                 ->values();
 
@@ -268,7 +262,7 @@ class FilamentPassportUiServiceProvider extends PackageServiceProvider
         $this->app->singleton(ConfigRepository::class);
         $this->app->singleton(
             ActionRepositoryContract::class,
-            fn (Application $app, array $params = []) => $this->makeRepository(
+            fn(Application $app, array $params = []) => $this->makeRepository(
                 app: $app,
                 params: $params,
                 repositoryClass: ActionRepository::class,
@@ -278,7 +272,7 @@ class FilamentPassportUiServiceProvider extends PackageServiceProvider
 
         $this->app->singleton(
             ResourceRepositoryContract::class,
-            fn (Application $app, array $params = []) => $this->makeRepository(
+            fn(Application $app, array $params = []) => $this->makeRepository(
                 app: $app,
                 params: $params,
                 repositoryClass: ResourceRepository::class,
