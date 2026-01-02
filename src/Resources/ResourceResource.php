ff3aac726280f37e5f4eb713f8bee3ffeb8487a3
Ilya Beliaev <info@php-dev.info>
2026-01-02 17:20:11 +0100

    feat: feature implementing

--- src/Resources/ResourceResource.php
+++ src/Resources/ResourceResource.php
@@ -7,4 +7,5 @@ namespace N3XT0R\FilamentPassportUi\Resources;
 class ResourceResource extends BaseManagementResource
 {
     protected static ?string $recordTitleAttribute = 'name';
+    protected static string|\UnitEnum|null $navigationGroup = 'filament-passport-ui::passport-ui.navigation.group';
 }
