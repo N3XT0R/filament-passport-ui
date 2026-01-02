f7d11951a2aceaaa86087ef800348491708772df
Ilya Beliaev <info@php-dev.info>
2026-01-02 17:21:30 +0100

    feat: feature implementing

--- src/Resources/PassportScopeResourceResource.php
+++ src/Resources/PassportScopeResourceResource.php
@@ -4,8 +4,11 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Resources;
 
-class ResourceResource extends BaseManagementResource
+use N3XT0R\FilamentPassportUi\Models\PassportScopeResource;
+
+class PassportScopeResourceResource extends BaseManagementResource
 {
+    protected static ?string $model = PassportScopeResource::class;
     protected static ?string $recordTitleAttribute = 'name';
     protected static string|\UnitEnum|null $navigationGroup = 'filament-passport-ui::passport-ui.navigation.group';
 }
