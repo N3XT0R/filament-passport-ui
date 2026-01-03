20cfcf09432aee389e0d0117c1d22bd4f39f0d4e
Ilya Beliaev <info@php-dev.info>
2026-01-03 14:57:45 +0100

    feat: feature implementing

--- src/Repositories/ScopeGrantRepository.php
+++ src/Repositories/ScopeGrantRepository.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Repositories;
 
+use Illuminate\Support\Collection;
 use N3XT0R\FilamentPassportUi\Models\Concerns\HasPassportScopeGrantsInterface;
 
 class ScopeGrantRepository
@@ -25,5 +26,17 @@ class ScopeGrantRepository
             ->where('resource_id', $resourceId)
             ->where('action_id', $actionId)
             ->exists();
+    }
+
+    /**
+     * Get all scope grants for the given tokenable.
+     * @param HasPassportScopeGrantsInterface $tokenable
+     * @return Collection
+     */
+    public function getTokenableGrants(HasPassportScopeGrantsInterface $tokenable): Collection
+    {
+        return $tokenable->passportScopeGrants()
+            ->with(['resource', 'action'])
+            ->get();
     }
 }
