907542b15268e472fb0d124f6bb8419a0e150b0f
Ilya Beliaev <info@php-dev.info>
2026-01-03 21:46:27 +0100

    feat: feature implementing

--- src/Resources/ClientResource/Pages/ViewClient.php
+++ src/Resources/ClientResource/Pages/ViewClient.php
@@ -5,9 +5,9 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Pages;
 
 use Filament\Actions\DeleteAction;
+use Filament\Actions\EditAction;
 use Filament\Resources\Pages\ViewRecord;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource;
-use N3XT0R\FilamentPassportUi\Resources\PassportScopeActionResource\Pages\EditAction;
 
 class ViewClient extends ViewRecord
 {
