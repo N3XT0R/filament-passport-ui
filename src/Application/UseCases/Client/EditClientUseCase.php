61941ca3e02d88c299bef968ba8a5ce3fd57799b
Ilya Beliaev <info@php-dev.info>
2026-01-04 00:51:31 +0100

    feat: feature implementing

--- src/Application/UseCases/Client/EditClientUseCase.php
+++ src/Application/UseCases/Client/EditClientUseCase.php
@@ -5,23 +5,42 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Application\UseCases\Client;
 
 use Illuminate\Contracts\Auth\Authenticatable;
+use N3XT0R\FilamentPassportUi\DTO\Client\OAuthClientData;
 use N3XT0R\FilamentPassportUi\Models\Passport\Client;
-use N3XT0R\FilamentPassportUi\Repositories\ClientRepository;
 use N3XT0R\FilamentPassportUi\Services\ClientService;
 use N3XT0R\FilamentPassportUi\Services\GrantService;
 
+/**
+ * Use case to edit an existing OAuth client
+ */
 readonly class EditClientUseCase
 {
     public function __construct(
         private ClientService $clientService,
-        private ClientRepository $clientRepository,
         private GrantService $grantService,
     ) {
     }
 
     public function execute(Client $client, array $data, ?Authenticatable $actor = null): Client
     {
-        $client->
-        $this->clientRepository->update($client);
+        $dto = new OAuthClientData(
+            name: $data['name'],
+            redirectUris: $data['redirect_uris'] ?? [],
+            provider: $data['provider'] ?? null,
+            confidential: $data['confidential'] ?? true,
+            options: $data['options'] ?? [],
+            revoked: $data['revoked'] ?? false,
+            owner: $data['owner'] ?? null,
+        );
+        $scopes = $data['scopes'] ?? [];
+
+        $client = $this->clientService->updateClient($client, $dto, $actor);
+        $this->grantService->upsertGrantsForTokenable(
+            tokenable: $client,
+            scopes: $scopes,
+            actor: $actor
+        );
+
+        return $client;
     }
 }
