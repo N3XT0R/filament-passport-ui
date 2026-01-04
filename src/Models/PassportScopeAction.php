dcd0a80c51dc2f39b149915264f5d7541c0fca7a
Ilya Beliaev <info@php-dev.info>
2026-01-05 00:03:59 +0100

    refactor: moved code from UI to Application/StateResolvers.

--- src/Models/PassportScopeAction.php
+++ src/Models/PassportScopeAction.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -24,6 +25,21 @@ class PassportScopeAction extends Model
     protected $casts = [
         'is_active' => 'bool',
     ];
+
+    /**
+     * Scope a query to only include global scope actions.
+     * @param Builder $query
+     * @param bool $isGlobal
+     * @return Builder
+     */
+    public function scopeIsGlobal(Builder $query, bool $isGlobal = true): Builder
+    {
+        if (!$isGlobal) {
+            $query->whereNotNull('resource_id');
+        }
+
+        return $query->whereNull('resource_id');
+    }
 
     public function resource(): BelongsTo
     {
