cad6565df20613dbc65f584d20520c6eb2cecdef
Ilya Beliaev <info@php-dev.info>
2026-01-04 16:07:09 +0100

    tests: added tests.

--- src/Observers/ClientObserver.php
+++ src/Observers/ClientObserver.php
@@ -6,6 +6,8 @@ namespace N3XT0R\FilamentPassportUi\Observers;
 
 use Illuminate\Database\Eloquent\Model;
 use N3XT0R\FilamentPassportUi\Events\Clients\OAuthClientCreated;
+use N3XT0R\FilamentPassportUi\Events\Clients\OAuthClientRevoked;
+use N3XT0R\FilamentPassportUi\Models\Passport\Client;
 use N3XT0R\FilamentPassportUi\Repositories\ClientRepository;
 use N3XT0R\FilamentPassportUi\Repositories\Scopes\ScopeGrantRepository;
 
@@ -20,5 +22,16 @@ class ClientObserver extends BaseObserver
     {
         app(ClientRepository::class)->delete($model);
         app(ScopeGrantRepository::class)->deleteAllGrantsForTokenable($model);
+    }
+
+    /**
+     * @param Client $model
+     * @return void
+     */
+    public function updated(Model $model): void
+    {
+        if ($model->getAttribute('revoked') === true) {
+            OAuthClientRevoked::dispatch($model);
+        }
     }
 }
