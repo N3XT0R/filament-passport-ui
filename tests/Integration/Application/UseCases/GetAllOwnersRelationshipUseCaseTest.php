17d4b54163f80ca251f8af7edfada172a8e96677
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2026-01-04 22:29:24 +0100

    Add integration tests for application use cases

---
+++ tests/Integration/Application/UseCases/GetAllOwnersRelationshipUseCaseTest.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\FilamentPassportUi\Tests\Integration\Application\UseCases;
+
+use App\Models\User;
+use N3XT0R\FilamentPassportUi\Application\UseCases\Owners\GetAllOwnersRelationshipUseCase;
+use N3XT0R\FilamentPassportUi\Tests\DatabaseTestCase;
+
+final class GetAllOwnersRelationshipUseCaseTest extends DatabaseTestCase
+{
+    private GetAllOwnersRelationshipUseCase $useCase;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $this->useCase = $this->app->make(GetAllOwnersRelationshipUseCase::class);
+    }
+
+    public function testExecuteReturnsOwnersWithLabels(): void
+    {
+        $first = User::factory()->create(['name' => 'First Owner']);
+        $second = User::factory()->create(['name' => 'Second Owner']);
+
+        config([
+            'passport-ui.owner_model' => $first::class,
+            'passport-ui.owner_label_attribute' => 'name',
+        ]);
+
+        $options = $this->useCase->execute();
+
+        self::assertSame(
+            [
+                $first->getKey() => 'First Owner',
+                $second->getKey() => 'Second Owner',
+            ],
+            $options->all()
+        );
+    }
+}
