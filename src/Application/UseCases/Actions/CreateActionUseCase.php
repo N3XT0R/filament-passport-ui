608d72a6ee91e9343dc7f80943fe382655f3d6cd
Ilya Beliaev <info@php-dev.info>
2026-01-06 00:28:12 +0100

    feat: introduce custom actions bound to use cases

--- src/Application/UseCases/Actions/CreateActionUseCase.php
+++ src/Application/UseCases/Actions/CreateActionUseCase.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Application\UseCases\Actions;
 
+use Illuminate\Contracts\Auth\Authenticatable;
 use N3XT0R\FilamentPassportUi\Models\PassportScopeAction;
 use N3XT0R\FilamentPassportUi\Services\ActionService;
-use RectorPrefix202512\Illuminate\Contracts\Auth\Authenticatable;
 
 readonly class CreateActionUseCase
 {
