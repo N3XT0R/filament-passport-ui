f9d1aed7e1614565ac18b20aa4ea7fd0168efba9
Ilya Beliaev <info@php-dev.info>
2026-01-05 02:16:05 +0100

    type cast.

--- src/Providers/Boot/PassportModelsBooter.php
+++ src/Providers/Boot/PassportModelsBooter.php
@@ -18,7 +18,7 @@ class PassportModelsBooter implements BooterInterface
 {
     public function boot(): void
     {
-        $config = config('passport-ui.models');
+        $config = (array)config('passport-ui.models');
 
         foreach ($config as $modelType => $modelClass) {
             if (empty($modelClass) && $modelType !== 'client') {
