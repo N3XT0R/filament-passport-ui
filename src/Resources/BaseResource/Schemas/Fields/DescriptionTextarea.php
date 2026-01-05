02286bf5ffc1f3c32e0230e82c25ee9812319b1f
Ilya Beliaev <info@php-dev.info>
2026-01-05 01:04:17 +0100

    feat: implemented interfaces schema and table classes.

--- src/Resources/BaseResource/Schemas/Fields/DescriptionTextarea.php
+++ src/Resources/BaseResource/Schemas/Fields/DescriptionTextarea.php
@@ -4,14 +4,14 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Resources\BaseResource\Schemas\Fields;
 
-use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Textarea;
 
-class DescriptionInput implements FieldInterface
+class DescriptionTextarea implements FieldInterface
 {
 
-    public static function make(string $name = 'description'): TextInput
+    public static function make(string $name = 'description'): Textarea
     {
-        return TextInput::make($name)
+        return Textarea::make($name)
             ->label(__('filament-passport-ui::passport-ui.common.description'));
     }
 }
