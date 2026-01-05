1b09a9850cb53dea0afef7139d6f2f5690d5c360
Ilya Beliaev <info@php-dev.info>
2026-01-05 20:38:07 +0100

    fix: filament in workbench fixed

--- workbench/bootstrap/providers.php
+++ workbench/bootstrap/providers.php
@@ -4,6 +4,5 @@
 return [
     App\Providers\WorkbenchServiceProvider::class,
     \Laravel\Passport\PassportServiceProvider::class,
-    \Filament\FilamentServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
 ];
