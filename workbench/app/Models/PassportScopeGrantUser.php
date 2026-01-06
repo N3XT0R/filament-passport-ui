f2ed3728699ff06310442877341f40297beceb0e
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2026-01-06 01:51:41 +0100

    Add integration tests for passport scope grants trait

---
+++ workbench/app/Models/PassportScopeGrantUser.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\UseFactory;
+use N3XT0R\FilamentPassportUi\Models\Concerns\HasPassportScopeGrantsInterface;
+use N3XT0R\FilamentPassportUi\Models\Traits\HasPassportScopeGrantsTrait;
+
+#[UseFactory(\Database\Factories\PassportScopeGrantUserFactory::class)]
+class PassportScopeGrantUser extends User implements HasPassportScopeGrantsInterface
+{
+    use HasPassportScopeGrantsTrait;
+}
