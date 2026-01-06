c296bd515594308a55e344dca3529a5d3ecdcb49
Ilya Beliaev <info@php-dev.info>
2026-01-06 21:40:02 +0100

    added revoke toggle.

--- src/Resources/ClientResource/Schemas/Fields/RevokeToggle.php
+++ src/Resources/ClientResource/Schemas/Fields/RevokeToggle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields;
 
 use Filament\Forms\Components\Toggle;
+use Illuminate\Database\Eloquent\Model;
 use N3XT0R\FilamentPassportUi\Resources\BaseResource\Schemas\Fields\FieldInterface;
 
 class RevokeToggle implements FieldInterface
@@ -13,6 +14,7 @@ class RevokeToggle implements FieldInterface
     public static function make(string $name = 'revoked'): Toggle
     {
         return Toggle::make($name)
-            ->label(__('filament-passport-ui::passport-ui.client_resource.form.revoke_label'));
+            ->label(__('filament-passport-ui::passport-ui.client_resource.form.revoke_label'))
+            ->visible(fn(?Model $record) => $record !== null);
     }
 }
