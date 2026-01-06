5a3af82f69ef4a0ad048dee1009c3f6f4ff280e7
Ilya Beliaev <N3XT0R@users.noreply.github.com>
2026-01-06 01:21:39 +0100

    Add scope display coverage to create client page tests

--- tests/Feature/Resources/ClientResource/Pages/CreateClientTest.php
+++ tests/Feature/Resources/ClientResource/Pages/CreateClientTest.php
@@ -7,6 +7,8 @@ namespace N3XT0R\FilamentPassportUi\Tests\Feature\Resources\ClientResource\Pages
 use App\Models\User;
 use Livewire\Livewire;
 use N3XT0R\FilamentPassportUi\Models\Passport\Client;
+use N3XT0R\FilamentPassportUi\Database\Factories\PassportScopeActionFactory;
+use N3XT0R\FilamentPassportUi\Database\Factories\PassportScopeResourceFactory;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource\Pages\CreateClient;
 use N3XT0R\FilamentPassportUi\Tests\DatabaseTestCase;
 
@@ -33,5 +35,35 @@ class CreateClientTest extends DatabaseTestCase
         $this->assertNotNull($client);
         $this->assertSame('New Client', $client->name);
         $this->assertNotNull(session()->get('new_client_secret_' . $client->getKey()));
+    }
+
+    public function testScopeResourcesAndActionsAreShownInForm(): void
+    {
+        config()->set('passport-ui.use_database_scopes', true);
+
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $ordersResource = PassportScopeResourceFactory::new()->create([
+            'name' => 'orders',
+        ]);
+        $paymentsResource = PassportScopeResourceFactory::new()->create([
+            'name' => 'payments',
+        ]);
+
+        $ordersAction = PassportScopeActionFactory::new()->withResource($ordersResource)->create([
+            'name' => 'read',
+        ]);
+        $globalAction = PassportScopeActionFactory::new()->create([
+            'name' => 'approve',
+        ]);
+
+        $component = Livewire::test(CreateClient::class);
+
+        $component->assertSeeText($ordersResource->name);
+        $component->assertSeeText($paymentsResource->name);
+
+        $component->assertSeeText($ordersResource->name . ':' . $ordersAction->name);
+        $component->assertSeeText($paymentsResource->name . ':' . $globalAction->name);
     }
 }
