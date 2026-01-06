8eb5bedd74e4ef78e6038827e331de4f9cb2a1d2
Ilya Beliaev <info@php-dev.info>
2026-01-06 20:24:58 +0100

    removed: ClientObserver.php

--- src/Providers/Boot/ObserverBooter.php
+++ src/Providers/Boot/ObserverBooter.php
@@ -4,10 +4,8 @@ declare(strict_types=1);
 
 namespace N3XT0R\FilamentPassportUi\Providers\Boot;
 
-use Laravel\Passport\Passport;
 use N3XT0R\FilamentPassportUi\Models\PassportScopeAction;
 use N3XT0R\FilamentPassportUi\Models\PassportScopeResource;
-use N3XT0R\FilamentPassportUi\Observers\ClientObserver;
 use N3XT0R\FilamentPassportUi\Observers\PassportScopeActionObserver;
 use N3XT0R\FilamentPassportUi\Observers\PassportScopeResourceObserver;
 
@@ -15,7 +13,6 @@ class ObserverBooter extends BaseBooter
 {
     public function boot(): void
     {
-        Passport::clientModel()::observe(ClientObserver::class);
         PassportScopeResource::observe(PassportScopeResourceObserver::class);
         PassportScopeAction::observe(PassportScopeActionObserver::class);
     }
