06f043575105e8275bf43e6f0af919f90b73d1e0
Ilya Beliaev <info@php-dev.info>
2026-01-07 18:16:51 +0100

    trash removed.

--- src/FilamentPassportUiServiceProvider.php
+++ src/FilamentPassportUiServiceProvider.php
@@ -10,7 +10,6 @@ use Illuminate\Support\Facades\Artisan;
 use Livewire\Features\SupportTesting\Testable;
 use N3XT0R\FilamentPassportUi\Commands\CleanupDatabaseCommand;
 use N3XT0R\FilamentPassportUi\Commands\ClearCacheCommand;
-use N3XT0R\FilamentPassportUi\Commands\FilamentPassportUiCommand;
 use N3XT0R\FilamentPassportUi\Database\Seeders\FilamentPassportUiDatabaseSeeder;
 use N3XT0R\FilamentPassportUi\Testing\TestsFilamentPassportUi;
 use Spatie\LaravelPackageTools\Commands\InstallCommand;
