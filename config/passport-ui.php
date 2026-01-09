5d7ac280975daf27f29ff8ae90ace3cfd71fba2c
Ilya Beliaev <info@php-dev.info>
2026-01-10 00:28:09 +0100

    refactor: use authorization core

--- config/passport-ui.php
+++ config/passport-ui.php
@@ -4,89 +4,12 @@ use Filament\Support\Icons\Heroicon;
 
 return [
     /**
-     * Model used as the owner of the OAuth2 clients.
-     */
-    'owner_model' => '\\App\\Models\\User',
-    'owner_label_attribute' => 'name',
-
-    /**
-     * Whether to use database stored scopes.
-     */
-    'use_database_scopes' => true,
-
-    /**
      * Navigation settings for the ClientResource.
      */
     'navigation' => [
         'client_resource' => [
             'group' => 'filament-passport-ui::passport-ui.navigation.group',
             'icon' => Heroicon::OutlinedKey,
-        ],
-    ],
-    /**
-     * Model mappings used by Passport and this package.
-     *
-     * Setting a value to `null` will fall back to Passport's default model.
-     */
-    'models' => [
-        /**Model used to represent OAuth2 auth_codes.
-         * Must be compatible with {@see \Laravel\Passport\AuthCode}.
-         */
-        'auth_code' => null,
-        /**
-         * Model used to represent OAuth2 clients.
-         *
-         * Must be compatible with {@see \N3XT0R\LaravelPassportAuthorizationCore\Models\Passport\Client}.
-         */
-        'client' => null,
-
-        /**
-         * Model used to represent OAuth2 tokens.
-         *
-         * Must be compatible with {@see \Laravel\Passport\Token}.
-         */
-        'token' => null,
-
-        /**
-         * Model used to represent OAuth2 scopes.
-         *
-         * Must be compatible with {@see \Laravel\Passport\Scope}.
-         */
-        'scope' => null,
-
-        /**
-         * Model used to represent OAuth2 auth codes.
-         *
-         * Must be compatible with {@see \Laravel\Passport\RefreshToken}.
-         */
-        'refresh_token' => null,
-    ],
-    /**
-     * Cache settings.
-     */
-    'cache' => [
-        /**
-         * Whether to cache the scopes.
-         */
-        'enabled' => true,
-
-        /**
-         * The cache ttl in seconds.
-         */
-        'ttl' => 3600,
-    ],
-
-    /**
-     * OAuth client settings and ui options.
-     */
-    'oauth' => [
-        'allowed_grant_types' => [
-            'authorization_code',
-            'client_credentials',
-            'password',
-            'personal_access',
-            'implicit',
-            'device',
         ],
     ],
 ];
