b6a0663f926ad4c1e3076c02be2c24fa028827fe
Ilya Beliaev <info@php-dev.info>
2026-01-11 10:50:02 +0100

    feat: add new resources.

---
+++ src/Resources/ClientResource/Schemas/CreateClientForm.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas;
+
+use Filament\Schemas\Components\Wizard;
+use Filament\Schemas\Schema;
+use Filament\Support\Icons\Heroicon;
+use N3XT0R\FilamentPassportUi\Resources\BaseResource\Schemas\FormInterface;
+use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\GrantTypeSelect;
+use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\NameInput;
+use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\OwnerSelect;
+
+class CreateClientForm implements FormInterface
+{
+    public static function configure(Schema $schema, array $additionalComponents = []): Schema
+    {
+        $components = [
+            Wizard::make([
+                Wizard\Step::make('client')
+                    ->icon(Heroicon::OutlinedKey)
+                    ->description(
+                        __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.description')
+                    )
+                    ->schema([
+                        NameInput::make(),
+                        OwnerSelect::make(),
+                        GrantTypeSelect::make(),
+                    ]),
+            ])
+                ->columnSpanFull(),
+        ];
+
+        return $schema->components(
+            array_merge(
+                $components,
+                $additionalComponents
+            )
+        );
+    }
+}
