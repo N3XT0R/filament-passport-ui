aa7d118dda088a18edc24b1537e7560cfb859f85
Ilya Beliaev <info@php-dev.info>
2026-01-11 18:52:58 +0100

    feat: add new form.

--- src/Resources/ClientResource/Schemas/CreateClientForm.php
+++ src/Resources/ClientResource/Schemas/CreateClientForm.php
@@ -5,9 +5,11 @@ declare(strict_types=1);
 namespace N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas;
 
 use Filament\Schemas\Components\Grid;
+use Filament\Schemas\Components\Utilities\Get;
 use Filament\Schemas\Components\Wizard;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
+use N3XT0R\FilamentPassportUi\Application\StateResolvers\GrantType\NeedsUserPermissionState;
 use N3XT0R\FilamentPassportUi\Resources\BaseResource\Schemas\FormInterface;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\GrantTypeSelect;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\NameInput;
@@ -26,12 +28,37 @@ class CreateClientForm implements FormInterface
                         __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.description')
                     )
                     ->schema([
+                        GrantTypeSelect::make('grant_type')
+                            ->live(),
                         Grid::make()
                             ->schema([
                                 NameInput::make(),
-                                OwnerSelect::make(),
+                                OwnerSelect::make()
+                                    ->required(function (Get $get): bool {
+                                        $grantType = $get('grant_type');
+
+                                        if ($grantType === null) {
+                                            return false;
+                                        }
+
+                                        return app(NeedsUserPermissionState::class)
+                                            ->execute($grantType);
+                                    }),
                             ]),
-                        GrantTypeSelect::make(),
+                    ]),
+                Wizard\Step::make('user_permission')
+                    ->visible(fn(Get $get) => app(NeedsUserPermissionState::class)->execute($get('grant_type')))
+                    ->label(
+                        __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.label')
+                    )
+                    ->icon(Heroicon::OutlinedUser)
+                    ->description(
+                        __(
+                            'filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.description'
+                        )
+                    )
+                    ->schema([
+
                     ]),
             ])->persistStepInQueryString()
                 ->columnSpanFull(),
