582b4a54a41499968cff0a3eb6b21c1525321459
Ilya Beliaev <info@php-dev.info>
2026-01-11 12:52:39 +0100

    feat: add new resources.

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -25,5 +25,7 @@ class DatabaseSeeder extends Seeder
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $this->call(\N3XT0R\LaravelPassportAuthorizationCore\Database\Seeders\DatabaseSeeder::class);
     }
 }
