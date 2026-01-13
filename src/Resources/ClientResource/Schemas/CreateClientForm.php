69f2c5d3ba10b4bc6370455e55c4c8c1b02e1f6b
Ilya Beliaev <info@php-dev.info>
2026-01-13 18:49:11 +0100

    added note.

--- src/Resources/ClientResource/Schemas/CreateClientForm.php
+++ src/Resources/ClientResource/Schemas/CreateClientForm.php
@@ -11,6 +11,7 @@ use Filament\Schemas\Components\Wizard;
 use Filament\Schemas\Schema;
 use Filament\Support\Icons\Heroicon;
 use N3XT0R\FilamentPassportUi\Application\StateResolvers\GrantType\NeedsUserPermissionState;
+use N3XT0R\FilamentPassportUi\Repositories\ConfigRepository;
 use N3XT0R\FilamentPassportUi\Resources\BaseResource\Components\ScopeCheckboxList;
 use N3XT0R\FilamentPassportUi\Resources\BaseResource\Schemas\FormInterface;
 use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\GrantTypeSelect;
@@ -19,7 +20,7 @@ use N3XT0R\FilamentPassportUi\Resources\ClientResource\Schemas\Fields\OwnerSelec
 
 class CreateClientForm implements FormInterface
 {
-    public function __construct()
+    public function __construct(protected ConfigRepository $configRepository)
     {
     }
 
@@ -36,62 +37,78 @@ class CreateClientForm implements FormInterface
 
     public function getComponents(): array
     {
+        $dbScopesRequired = $this->configRepository->isUsingDatabaseScopes();
+        $steps = [
+            Wizard\Step::make('client')
+                ->label(__('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.label'))
+                ->icon(Heroicon::OutlinedKey)
+                ->description(
+                    __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.description')
+                )
+                ->schema($this->getClientComponents())
+        ];
+
+        if ($dbScopesRequired) {
+            $steps[] = Wizard\Step::make('user_permission')
+                ->visible(fn(Get $get) => app(NeedsUserPermissionState::class)->execute($get('grant_type')))
+                ->label(
+                    __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.label')
+                )
+                ->icon(Heroicon::OutlinedUser)
+                ->description(
+                    __(
+                        'filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.description'
+                    )
+                )
+                ->schema($this->getUserPermissionComponents());
+        }
+
+
         return [
-            Wizard::make([
-                Wizard\Step::make('client')
-                    ->label(__('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.label'))
-                    ->icon(Heroicon::OutlinedKey)
-                    ->description(
-                        __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.client.description')
-                    )
-                    ->schema($this->getClientComponents()),
-                Wizard\Step::make('user_permission')
-                    ->visible(fn(Get $get) => app(NeedsUserPermissionState::class)->execute($get('grant_type')))
-                    ->label(
-                        __('filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.label')
-                    )
-                    ->icon(Heroicon::OutlinedUser)
-                    ->description(
-                        __(
-                            'filament-passport-ui::passport-ui.client_resource.form.wizard.steps.user_permission.description'
-                        )
-                    )
-                    ->schema($this->getUserPermissionComponents()),
-            ])->persistStepInQueryString()
-                ->columnSpanFull(),
+            Wizard::make()
+                ->steps($steps)
+                ->columnSpanFull()
+                ->persistStepInQueryString(),
         ];
     }
 
     public function getClientComponents(): array
     {
+        $dbScopesRequired = $this->configRepository->isUsingDatabaseScopes();
+        $components = [
+            NameInput::make()
+                ->unique('oauth_clients', 'name'),
+            OwnerSelect::make()
+                ->required(function (Get $get): bool {
+                    $grantType = $get('grant_type');
+
+                    if ($grantType === null) {
+                        return false;
+                    }
+
+                    return app(NeedsUserPermissionState::class)
+                        ->execute($grantType);
+                }),
+        ];
+
+        if ($dbScopesRequired) {
+            $components[] = Grid::make()
+                ->schema([
+                    ScopeCheckboxList::make(
+                        context: 'client',
+                        name: 'client_scopes',
+                        statePath: 'client_scopes',
+                    )
+                ])
+                ->columnSpanFull();
+        }
+
+
         return [
             GrantTypeSelect::make('grant_type')
                 ->live(),
             Grid::make()
-                ->schema([
-                    NameInput::make()
-                        ->unique('oauth_clients', 'name'),
-                    OwnerSelect::make()
-                        ->required(function (Get $get): bool {
-                            $grantType = $get('grant_type');
-
-                            if ($grantType === null) {
-                                return false;
-                            }
-
-                            return app(NeedsUserPermissionState::class)
-                                ->execute($grantType);
-                        }),
-                    Grid::make()
-                        ->schema([
-                            ScopeCheckboxList::make(
-                                context: 'client',
-                                name: 'client_scopes',
-                                statePath: 'client_scopes',
-                            )
-                        ])
-                        ->columnSpanFull()
-                ]),
+                ->schema($components),
         ];
     }
 
